71282d4b0f9f755ce8fc625980f801bd34af826a
Santiago Manrique <102564125+SantiMs15@users.noreply.github.com>
2022-05-29 14:25:13 -0500

    Add files via upload

--- Code/Reader.py
+++ Code/Reader.py
@@ -1,3 +1,9 @@
 import pyttsx3
-speaker = pyttsx3
-speaker.speak('Haciendo tarea en la casa desde hace 3 días')
+import PyPDF2
+libro = open('pnl_cast.pdf','rb')
+PdfReader = PyPDF2.PdfFileReader(libro)
+paginas = PdfReader.numPages
+print(paginas)
+speaker = pyttsx3.init()
+speaker.say('Haciendo tarea en la casa desde hace 3 días')
+speaker.runAndWait()

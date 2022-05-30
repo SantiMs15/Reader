a2e21e374d65c31a448ecf468fa9b13f15ece6de
David Santiago Guzmán Ruiz <adanbot23@gmail.com>
2022-05-29 19:48:52 -0500

    nuevos generos

--- Code/Reader.py
+++ Code/Reader.py
@@ -30,7 +30,7 @@ if a=="leyenda":
 
 #Terror
 if a=="terror":
-    libro=open('C:\Users\USUARIO\curseforge\Desktop\Reader project\Reader\libros\it-eso','rb')
+    libro=open('C:\Reader project\Reader\libros\it-eso','rb')
     PdfReader=PyPDF2.PdfFileReader(libro)
     paginas=PdfReader.numPages
     print(f'Estas escuchanda It el payaso de Stephen King. Nro de paginas:{paginas}')
@@ -40,3 +40,29 @@ if a=="terror":
         text=page.extract_text()
         speaker.say(text)
         speaker.runAndWait()
+
+#Fantasia
+    libro=open()
+    PdfReader=PyPDF2.PdfFileReader(libro)
+    paginas=PdfReader.numPages
+    print(f'Estas escuchando Percy Jackson el Ladron del rayo de Rick Riordan. Nro de paginas:{paginas}')
+    speaker=pyttsx3.init()
+    for paginas in range (0, paginas):
+        page=PdfReader.getPage(0)
+        text=page.extract_text()
+        speaker.say(text)
+        speaker.runAndWait()
+
+#Drama
+    libro=open()
+    PdfReader=PyPDF2.PdfFileReader(libro)
+    paginas=PdfReader.numPages
+    print(f'Estas escuchando El retrato de Dorian Gray de Oscar Wilde:{paginas}')
+    speaker=pyttsx3.init()
+    for paginas in range (0, paginas):
+        page=PdfReader.getPage(0)
+        text=page.extract_text()
+        speaker.say(text)
+        speaker.runAndWait()
+
+#Romance
